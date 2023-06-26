2bac481825d70c518451c701ba11573fad3c6127
Swayy03 <sweetmuffin03@gmail.com>
2023-06-26 10:03:14 +0300

    Finished RFID door
    With this code, the RFID door authentication system is complete.
    
    The steps are custom hence might not work on anyone else's door or system thereof.

--- RFIDCode/RFIDCode.ino/RFIDCode.ino.ino
+++ RFIDCode/RFIDCode.ino/RFIDCode.ino.ino
@@ -1,18 +1,24 @@
+#include <SPI.h>
+#include <MFRC522.h>
+#include <Stepper.h>
+
 int buzzPin = 2;
 int greenPin = 7;
 int redPin = 4;
 int delayT = 200;
-int delayT1 = 400;
- 
-#include <SPI.h>
-#include <MFRC522.h>
+int delayT1 = 600;
+int stepsPerRevolution = 2048;
+int motSpeed = 10;
+int count = 0;
+int ForceOpen = 0;
  
 #define SS_PIN 10
 #define RST_PIN 9
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
- 
+Stepper myStepper(stepsPerRevolution,8,5,3,6);
 void setup() 
 {
+  myStepper.setSpeed(motSpeed);
   pinMode(greenPin,OUTPUT);
   pinMode(redPin,OUTPUT);
   pinMode(buzzPin,OUTPUT);
@@ -21,7 +27,9 @@ void setup()
   mfrc522.PCD_Init();   // Initiate MFRC522
 //  Serial.println("Approximate your card to the reader...");
   Serial.println();
-
+ 
+  
+ // myStepper.step(50);
 }
 void loop() 
 {
@@ -50,36 +58,86 @@ void loop()
   Serial.println();
   Serial.print("Message : ");
   content.toUpperCase();
-  if (content.substring(1) == "17 86 0F 3C") //change here the UID of the card/cards that you want to give access
+  
+  if (content.substring(1) == "17 86 0F 3C" && count%2 == 1) //change here the UID of the card/cards that you want to give access
   {
     Serial.println("Samson's Card Authorized access");
     Serial.println();
     digitalWrite(greenPin,HIGH);
+    digitalWrite(redPin,HIGH);                                                                                                                              
+    myStepper.step(2248);
+    myStepper.step(-200);
+    delay(delayT1);
     digitalWrite(buzzPin,HIGH);
     delay(delayT1);
-    digitalWrite(greenPin,LOW);
+    digitalWrite(greenPin,HIGH);
+    digitalWrite(redPin,LOW);
     digitalWrite(buzzPin,LOW);
+    count++;
+    
+  
     
   }
+  else if(content.substring(1) == "5B 10 63 1B" && count%2 == 0){
+      digitalWrite(greenPin,HIGH);
+      digitalWrite(redPin,HIGH);
+      myStepper.step(-2200);
+      digitalWrite(greenPin,LOW);
+      digitalWrite(buzzPin,HIGH);
+      delay(delayT1);
+      digitalWrite(buzzPin,LOW);
+      count++;
+  }
+
+  
+  else if(content.substring(1) == "17 86 0F 3C"){
+    ForceOpen++;
+    digitalWrite(redPin,HIGH);
+    digitalWrite(buzzPin,HIGH);
+    delay(delayT);
+    digitalWrite(redPin,LOW);
+    digitalWrite(buzzPin,LOW);
+    delay(delayT);
+    digitalWrite(greenPin,HIGH);
+    digitalWrite(buzzPin,HIGH);
+    delay(delayT);
+    digitalWrite(greenPin,LOW);
+    digitalWrite(buzzPin,LOW);
+    delay(delayT);
+    digitalWrite(redPin,HIGH);
+    digitalWrite(buzzPin,HIGH);
+    delay(delayT);
+    digitalWrite(redPin,LOW);
+    digitalWrite(buzzPin,LOW);
+    delay(delayT);
+    if(ForceOpen == 4){
+        myStepper.step(2048);
+      }
+      if(ForceOpen == 5){
+        myStepper.step(-2048);
+        ForceOpen = 0;
+      }
+  }
+  
   else 
   {
      Serial.println("Access denied");
     Serial.println();
     digitalWrite(redPin,HIGH);
     digitalWrite(buzzPin,HIGH);
-    delay(delayT1);
+    delay(delayT);
     digitalWrite(redPin,LOW);
     digitalWrite(buzzPin,LOW);
-    delay(delayT1);
+    delay(delayT);
     digitalWrite(redPin,HIGH);
     digitalWrite(buzzPin,HIGH);
-    delay(delayT1);
+    delay(delayT);
     digitalWrite(redPin,LOW);
     digitalWrite(buzzPin,LOW);
-    delay(delayT1);
+    delay(delayT);
     digitalWrite(redPin,HIGH);
     digitalWrite(buzzPin,HIGH);
-    delay(delayT1);
+    delay(delayT);
     digitalWrite(redPin,LOW);
     digitalWrite(buzzPin,LOW);
    
